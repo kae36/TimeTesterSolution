fd1bf5351a310e88c12c615a8b1ccd66e55f190e
Alan Edwards <kae36@users.noreply.github.com>
2018-03-05 09:57:22 -0500

    Initial Commit
    Initial Commit to GitHub

---
+++ TimeTester/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace TimeTester
+{
+    internal class Program
+    {
+        public const string DayTimeConnectionString = "DayTime";
+        public const string NightTimeConnectionString = "NightTime";
+        public const string NightTimeStart = "22:00:00";
+        public const string NightTimeEnd = "06:30:00";
+
+        static void Main()
+        {
+            var nightTimeStart = TimeKeeper.ToTimeSpan(NightTimeStart);
+            var nightTimeEnd = TimeKeeper.ToTimeSpan(NightTimeEnd);
+
+            var connection = new ConnectionStringProvider(DayTimeConnectionString, NightTimeConnectionString, nightTimeStart, nightTimeEnd, DateTime.Now.TimeOfDay);
+
+            var which = connection.GetConnectionString();
+
+            Console.WriteLine($"Connection String is '{which}' at {DateTime.Now}");
+        }
+        
+    }
+
+    public class TimeKeeper
+    {
+        public static TimeSpan ToTimeSpan(string nightTimeStart)
+        {
+            string[] timeParts = nightTimeStart.Split(new[] {':'}, 3);
+            return new TimeSpan(Convert.ToInt32(timeParts[0]),
+                Convert.ToInt32(timeParts[1]),
+                Convert.ToInt32(timeParts[2]));
+        }
+    }
+
+    public class ConnectionStringProvider
+    {
+        public TimeSpan CurrentTime { get; }
+        private readonly string _dayTimeConnectionString;
+        private readonly string _nightTimeConnectionString;
+        private readonly TimeSpan _nightTimeStart;
+        private readonly TimeSpan _nightTimeEnd;
+        private readonly TimeSpan _currentTime;
+
+        public ConnectionStringProvider(string dayTimeConnectionString,
+            string nightTimeConnectionString,
+            TimeSpan nightTimeStart,
+            TimeSpan nightTimeEnd,
+            TimeSpan currentTime)
+        {
+            CurrentTime = currentTime;
+            _dayTimeConnectionString = dayTimeConnectionString;
+            _nightTimeConnectionString = nightTimeConnectionString;
+            _nightTimeStart = nightTimeStart;
+            _nightTimeEnd = nightTimeEnd;
+            _currentTime = currentTime;
+        }
+
+        public string GetConnectionString()
+        {
+            return IsNightTime() ? _nightTimeConnectionString : _dayTimeConnectionString;
+        }
+
+        private bool IsNightTime()
+        {
+            var currentTime = _currentTime; // DateTime.Now.TimeOfDay;
+            var dssNightTimeStart = _nightTimeStart;
+            var dssNightTimeEnd = _nightTimeEnd;
+            var isNightTime = ((currentTime.Hours > 12) && (currentTime.CompareTo(dssNightTimeStart) >= 0)) ||
+                              ((currentTime.Hours < 12) && (currentTime.CompareTo(dssNightTimeEnd) <= 0));
+            return isNightTime;
+        }
+    }
+}
---
+++ TimeTesterSolution.Tests/TestStringReturned.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using TimeTester;
+using Xunit;
+
+namespace TimeTesterSolution.Tests
+{
+    [Trait("Test","")]
+    public class TestStringReturned
+    {
+        public const string DayTimeConnectionString = "DayTime";
+        public const string NightTimeConnectionString = "NightTime";
+        public readonly TimeSpan NightTimeStart = TimeKeeper.ToTimeSpan("22:00:00");
+        public readonly TimeSpan NightTimeEnd = TimeKeeper.ToTimeSpan("06:30:00");
+
+        [Theory]
+        [InlineData("21:59:59", "DayTime")]
+        [InlineData("22:00:00", "NightTime")]
+        [InlineData("22:00:01", "NightTime")]
+        [InlineData("23:00:00", "NightTime")]
+        [InlineData("00:00:00", "NightTime")]
+        [InlineData("01:00:00", "NightTime")]
+        [InlineData("02:00:00", "NightTime")]
+        [InlineData("03:00:00", "NightTime")]
+        [InlineData("04:00:00", "NightTime")]
+        [InlineData("05:00:00", "NightTime")]
+        [InlineData("06:00:00", "NightTime")]
+        [InlineData("06:29:59", "NightTime")]
+        [InlineData("06:30:00", "NightTime")]
+        [InlineData("06:30:01", "DayTime")]
+        [InlineData("07:00:00", "DayTime")]
+        [InlineData("08:00:00", "DayTime")]
+        [InlineData("09:00:00", "DayTime")]
+        [InlineData("10:00:00", "DayTime")]
+        [InlineData("11:00:00", "DayTime")]
+        [InlineData("12:00:00", "DayTime")]
+        [InlineData("13:00:00", "DayTime")]
+        [InlineData("14:00:00", "DayTime")]
+        [InlineData("15:00:00", "DayTime")]
+        [InlineData("16:00:00", "DayTime")]
+        [InlineData("17:00:00", "DayTime")]
+        [InlineData("18:00:00", "DayTime")]
+        [InlineData("19:00:00", "DayTime")]
+        [InlineData("20:00:00", "DayTime")]
+        [InlineData("21:00:00", "DayTime")]
+        public void StringReturnedMatchesExpected(string time, string expected)
+        {
+            // Arrange
+            var now = TimeKeeper.ToTimeSpan(time);
+            var csp = new ConnectionStringProvider(DayTimeConnectionString, NightTimeConnectionString, NightTimeStart,
+                NightTimeEnd, now);
+            // Act
+            var value = csp.GetConnectionString();
+            // Assert
+            Assert.Equal(value, expected);
+        }
+    }
+}
